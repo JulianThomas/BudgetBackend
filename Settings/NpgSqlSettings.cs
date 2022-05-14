48c80311ea727a826ff4023b25ffea5f4e4caa85
Julian Thomas <5982589+JulianThomas@users.noreply.github.com>
2022-05-14 12:03:16 +0530

    npgsql

---
+++ Settings/NpgSqlSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace Budget.Settings
+{
+    public class NpgSqlSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Database { get; set; }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return $"Server={Host};Database={Database};Port={Port};User Id={Username};Password={Password}";
+            }
+        }
+    }
+}
